1a56edf97a758813d9fe3896b76b956b1ddc9a2a
fayoo19 <fayazaly19@gmail.com>
2022-11-03 10:28:26 -0700

    loops

---
+++ app.js
@@ -0,0 +1,61 @@
+// while loop
+
+// let a = 1
+// while(a<=100){
+//     document.write (a , "<br>" ) 
+//     a = a+1 
+// }
+
+// let a = -100
+// while(a<=100){
+//     document.write ( a , "<br>"   )
+//     a = a+1
+// }
+
+// DO WHILE
+
+//     let c = 1 
+//     do {
+//         document.write(c+  "<br>")
+//         c = c + 1 ;
+//     }while(c <= 100)
+
+    
+// FOR LOOP
+// for(let d = 1;d<=10;d++ ){
+//     document.write( d , "<br>"   )
+// }
+
+// let g = 3 
+// while(g<=60){
+//     document.write(g+ , "<br>")
+//     g = g++
+// }
+// let h = 1
+// do{ document.write(g+  "<br>")
+// g = g+1 ;
+// } while (g<=10)
+
+// for(let d = 10;d<=20;d++ ){
+//         document.write( d , "<br>"   )}
+
+//  let c = 20 
+//      do {
+//          document.write(c - "<br>")
+//          c = c - 1 ;
+//      }while(c >= 10)
+
+
+//     EVEN LOOOP
+
+//     for(let a =1;a<=100;a++){
+//         if(a % 2 == 0){
+//             document.write(a ,"<br>")
+//         }
+//     }
+// EVEN LOOP
+//     for(let k =3;k<=12;k++){
+//         if(k % 3 === 0){
+//             document.write(k ,"<br>")
+//         }
+//     }     
